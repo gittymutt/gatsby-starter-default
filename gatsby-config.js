9ca640486ce1d039613043c8dc267405fb00eafb
gittymutt <pedroschmitz@hotmail.com>
2022-01-12 15:37:26 -0500

    add wpplugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,16 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+      
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+          url:
+              process.env.WPGRAPHQL_URL ||
+              `https://diffuse-mode.ml/graphql`,
+      },
+  }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
