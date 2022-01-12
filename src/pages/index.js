319c7ba48e449b0f416abf5e7b2fe34f4c3b67a4
Peter Schmitz <pedroschmitz@hotmail.com>
2022-01-12 15:05:02 -0500

    CHange message

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Vote Big Buddy for President</h1>
+    <p>You voted for one before.</p>
+    <p>Might as well vote for a real one this time.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
